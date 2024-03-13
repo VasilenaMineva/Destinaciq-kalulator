a1063b7a6aef1fbda7841e756e7099db59b824bf
vasilena_mineva <74876631+VasilenaMineva@users.noreply.github.com>
2024-03-13 12:42:51 +0200

    Add files via upload

---
+++ favouriteChanel/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace favouriteChanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.началоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.услугиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.времетраенеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.модниТенденцииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.изпълнителToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.стилToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.каналToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.минутиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.минутиToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.минутиToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.таToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.натаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.таToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.таToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Table;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 296);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(593, 4);
+            this.menuStrip1.TabIndex = 0;
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.началоToolStripMenuItem,
+            this.услугиToolStripMenuItem,
+            this.времетраенеToolStripMenuItem,
+            this.модниТенденцииToolStripMenuItem,
+            this.изпълнителToolStripMenuItem,
+            this.стилToolStripMenuItem,
+            this.каналToolStripMenuItem});
+            this.menuStrip2.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Table;
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(593, 296);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // началоToolStripMenuItem
+            // 
+            this.началоToolStripMenuItem.BackColor = System.Drawing.Color.Purple;
+            this.началоToolStripMenuItem.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.началоToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.началоToolStripMenuItem.Name = "началоToolStripMenuItem";
+            this.началоToolStripMenuItem.Size = new System.Drawing.Size(79, 39);
+            this.началоToolStripMenuItem.Text = "Начало";
+            // 
+            // услугиToolStripMenuItem
+            // 
+            this.услугиToolStripMenuItem.BackColor = System.Drawing.Color.Purple;
+            this.услугиToolStripMenuItem.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.услугиToolStripMenuItem.ForeColor = System.Drawing.Color.FloralWhite;
+            this.услугиToolStripMenuItem.Name = "услугиToolStripMenuItem";
+            this.услугиToolStripMenuItem.Size = new System.Drawing.Size(73, 39);
+            this.услугиToolStripMenuItem.Text = "Услуги";
+            // 
+            // времетраенеToolStripMenuItem
+            // 
+            this.времетраенеToolStripMenuItem.BackColor = System.Drawing.Color.Purple;
+            this.времетраенеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.минутиToolStripMenuItem,
+            this.минутиToolStripMenuItem1,
+            this.минутиToolStripMenuItem2});
+            this.времетраенеToolStripMenuItem.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.времетраенеToolStripMenuItem.ForeColor = System.Drawing.Color.FloralWhite;
+            this.времетраенеToolStripMenuItem.Name = "времетраенеToolStripMenuItem";
+            this.времетраенеToolStripMenuItem.Size = new System.Drawing.Size(120, 39);
+            this.времетраенеToolStripMenuItem.Text = "Времетраене";
+            // 
+            // модниТенденцииToolStripMenuItem
+            // 
+            this.модниТенденцииToolStripMenuItem.BackColor = System.Drawing.Color.Purple;
+            this.модниТенденцииToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.таToolStripMenuItem,
+            this.натаToolStripMenuItem,
+            this.таToolStripMenuItem1,
+            this.таToolStripMenuItem2});
+            this.модниТенденцииToolStripMenuItem.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.модниТенденцииToolStripMenuItem.ForeColor = System.Drawing.Color.FloralWhite;
+            this.модниТенденцииToolStripMenuItem.Name = "модниТенденцииToolStripMenuItem";
+            this.модниТенденцииToolStripMenuItem.Size = new System.Drawing.Size(156, 39);
+            this.модниТенденцииToolStripMenuItem.Text = "Модни тенденции";
+            // 
+            // изпълнителToolStripMenuItem
+            // 
+            this.изпълнителToolStripMenuItem.BackColor = System.Drawing.Color.Purple;
+            this.изпълнителToolStripMenuItem.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.изпълнителToolStripMenuItem.ForeColor = System.Drawing.Color.FloralWhite;
+            this.изпълнителToolStripMenuItem.Name = "изпълнителToolStripMenuItem";
+            this.изпълнителToolStripMenuItem.Size = new System.Drawing.Size(115, 39);
+            this.изпълнителToolStripMenuItem.Text = "Изпълнител";
+            // 
+            // стилToolStripMenuItem
+            // 
+            this.стилToolStripMenuItem.BackColor = System.Drawing.Color.Purple;
+            this.стилToolStripMenuItem.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.стилToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.стилToolStripMenuItem.Name = "стилToolStripMenuItem";
+            this.стилToolStripMenuItem.Size = new System.Drawing.Size(65, 39);
+            this.стилToolStripMenuItem.Text = "Стил";
+            // 
+            // каналToolStripMenuItem
+            // 
+            this.каналToolStripMenuItem.BackColor = System.Drawing.Color.Purple;
+            this.каналToolStripMenuItem.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.каналToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.каналToolStripMenuItem.Name = "каналToolStripMenuItem";
+            this.каналToolStripMenuItem.Size = new System.Drawing.Size(70, 39);
+            this.каналToolStripMenuItem.Text = "Канал";
+            // 
+            // минутиToolStripMenuItem
+            // 
+            this.минутиToolStripMenuItem.Name = "минутиToolStripMenuItem";
+            this.минутиToolStripMenuItem.Size = new System.Drawing.Size(180, 40);
+            this.минутиToolStripMenuItem.Text = "3 минути";
+            // 
+            // минутиToolStripMenuItem1
+            // 
+            this.минутиToolStripMenuItem1.Name = "минутиToolStripMenuItem1";
+            this.минутиToolStripMenuItem1.Size = new System.Drawing.Size(180, 40);
+            this.минутиToolStripMenuItem1.Text = "4 минути";
+            // 
+            // минутиToolStripMenuItem2
+            // 
+            this.минутиToolStripMenuItem2.Name = "минутиToolStripMenuItem2";
+            this.минутиToolStripMenuItem2.Size = new System.Drawing.Size(180, 40);
+            this.минутиToolStripMenuItem2.Text = "5 минути";
+            // 
+            // таToolStripMenuItem
+            // 
+            this.таToolStripMenuItem.Name = "таToolStripMenuItem";
+            this.таToolStripMenuItem.Size = new System.Drawing.Size(180, 40);
+            this.таToolStripMenuItem.Text = "90-те";
+            // 
+            // натаToolStripMenuItem
+            // 
+            this.натаToolStripMenuItem.Name = "натаToolStripMenuItem";
+            this.натаToolStripMenuItem.Size = new System.Drawing.Size(180, 40);
+            this.натаToolStripMenuItem.Text = "2000-ната";
+            // 
+            // таToolStripMenuItem1
+            // 
+            this.таToolStripMenuItem1.Name = "таToolStripMenuItem1";
+            this.таToolStripMenuItem1.Size = new System.Drawing.Size(180, 40);
+            this.таToolStripMenuItem1.Text = "2010-та";
+            // 
+            // таToolStripMenuItem2
+            // 
+            this.таToolStripMenuItem2.Name = "таToolStripMenuItem2";
+            this.таToolStripMenuItem2.Size = new System.Drawing.Size(180, 40);
+            this.таToolStripMenuItem2.Text = "2020-та";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(593, 321);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Любимото каналче";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem началоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem услугиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem времетраенеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem минутиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem минутиToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem минутиToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem модниТенденцииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem таToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem натаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem таToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem таToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem изпълнителToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem стилToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem каналToolStripMenuItem;
+    }
+}
+
